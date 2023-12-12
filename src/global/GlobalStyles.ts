520c03cceafbe83f1af66d60bc8cf4f3d2477d24
Leonardo Lima <leonardo.b.lima1@gmail.com>
2023-12-12 14:22:02 -0300

    primeiro commit

---
+++ src/global/GlobalStyles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+html, body, #root {
+  min-height: 100vh;
+} 
+
+
+* {
+ padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Sora', sans-serif;
+}
+`;
---
+++ src/global/Theme.ts
@@ -0,0 +1,12 @@
+export const Theme = {
+  colors: {
+    "brand-color": "#e51d37",
+    dark10: "#646466",
+    dark20: "#333333",
+    support1: "#2962ff",
+    support2: "#0bb07b",
+    support3: "#ffce52",
+    support4: "#f03d3d",
+    support5: "#f9f9f9",
+  },
+};
